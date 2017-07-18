a8c1f220e5ab2fb6eba653011d73f0325bd545a4
evaseemefly <evaseemefly@126.com>
2017-07-18 20:48:51 +0800

    7月18日首次创建

---
+++ MyExample_DutyManagementSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyExample_DutyManagementSystem.MvcApplication" Language="C#" %>
---
+++ MyExample_DutyManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyExample_DutyManagementSystem.Startup))]
+namespace MyExample_DutyManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
